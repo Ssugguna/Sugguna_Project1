1fd5f6595e001d5e1f4522d32b4e3371dd526bbc
Ssugguna <39936955+Ssugguna@users.noreply.github.com>
2018-06-05 06:30:06 -0400

    removed author

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,9 +4,7 @@ import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by ashish on 13/5/17.
- */
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 }
