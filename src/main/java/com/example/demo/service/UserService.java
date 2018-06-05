c98ba3098ccbd4ab16b75ae69597da7816c47733
Ssugguna <39936955+Ssugguna@users.noreply.github.com>
2018-06-05 00:22:45 -0400

    Code changes for business logic

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.dto.UserDto;
+
+
+public interface UserService {
+    UserDto getUserById(Integer userId);
+    void saveUser(UserDto userDto);
+    List<UserDto> getAllUsers();
+}
