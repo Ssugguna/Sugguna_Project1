836442fa9de20fc402e0ae059152b297f79f801d
Ssugguna <39936955+Ssugguna@users.noreply.github.com>
2018-06-05 06:29:41 -0400

    removed author

--- src/main/java/com/example/demo/repository/SkillRepository.java
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -4,9 +4,6 @@ import com.example.demo.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by ashish on 13/5/17.
- */
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Integer>{
 }
